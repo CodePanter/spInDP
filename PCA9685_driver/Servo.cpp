8b5218353696d307361108d92bec526b5594f9c1
Sietsewer <sietsewer@gmail.com>
2014-05-27 12:08:46 +0200

    drivers update

--- PCA9685_driver/Servo.cpp
+++ PCA9685_driver/Servo.cpp
@@ -6,11 +6,17 @@
 //Constructor
 
 Servo::Servo(int channel, PCA9685 driver, int min, int max) {
+    this->running = true;
+    this->turning = false;
+    this->servo_angle = 0;
+    this->servo_speed = 1000; // DURATION IN MS
     this->channel = channel;
     this->driver = &driver;
     this->driver->setPWMFreq(FREQ);
     this->min_angle = min;
     this->max_angle = max;
+    this->max_length_mod = MAX_LENGTH;
+    this->min_length_mod = MIN_LENGTH;
 
     this->thread_parameters = {
         &this,
@@ -29,6 +35,10 @@ Servo::~Servo() {
     pthread_join(thread, NULL); // Sleep until thread ends.
 }
 
+PCA9685 * Servo::getDriver() {
+    return this->driver;
+}
+
 int Servo::isRunning() {
     return this->running;
 }
@@ -37,30 +47,57 @@ int Servo::isTurning() {
     return this->turning;
 }
 
+int Servo::getMaxLength() {
+    return this->max_length_mod;
+}
+
+int Servo::getMinLength() {
+    return this->min_length_mod;
+}
+
 int Servo::setAngle(int angle, int speed) {
-/* EDIT TO USE THREAD */
+
     if ((angle > this->max_angle) || (angle < this->min_angle) {
             return 1; // INCORRECT PARAMETERS
         }
-
-    float angleMod = (float) (angle + 90) / 180; // Make angle 0.0f to 1.0f.
-            int paramA = (int) ((max_length_mod - min_length_mod) * angleMod) + min_length_mod;
-            driver->setPWM(channel, 0, paramA);
+    this->targetAngle = angle;
+            this->speed = speed;
 
     return 0; // DONE
 }
 
-void* thread_method(void* args) {
+void* Servo::thread_method(void* args) {
     ThreadParameters* args_struct = (ThreadParameters *) args;
+
             Servo* servo = args_struct->servo;
+            PCA9685* driver = args_struct->driver;
             int* targetAngle = args_struct->targetAngle;
             int* speed = args_struct->speed;
             bool* turning = args_struct->turning;
             bool* running = args_struct->running;
 
+            float currentAngle = 0.0f;
+            float angleMod = 0.0f;
+            int currentTargetAngle = 0;
+            float angleDif = 0.0f;
+            int ticksLeft;
+            int pwmFreq = servo.MAX_LENGTH;
 
-            /* ADD STUFF? */
+
     while (servo->isRunning()) {
-        /* ADD SERVO LOOP LOGIC */
+        if (!(*targetAngle == currentTargetAngle)) {
+            // Calculate difference per ms
+            angleDif = (((float) currentTargetAngle - (float) *targetAngle) / speed);
+        }
+        if (currentAngle != targetAngle) {
+            *turning = true; // Servo is turning.
+                    currentAngle += angleDif; // Add angle for this tick to current angle.
+                    angleMod = (float) (currentAngle + 90) / 180;
+                    pwmFreq = (int) ((max_length_mod - min_length_mod) * angleMod) + min_length_mod;
+                    driver->setPWM(channel, 0, paramA);
+        } else {
+            *turning = false; // Servo is stationary.
+        }
+        usleep(1000); // sleep for 1ms.
     }
 }
--- PCA9685_driver/Servo.h
+++ PCA9685_driver/Servo.h
@@ -19,37 +19,40 @@ public:
     int setPeriod(int);
     bool isRunning(void);
     bool isTurning(void);
+    int getMaxLength(void);
+    int getMinLength(void);
 
 private:
 
     struct ThreadParameters{
         Servo* servo;
+        PCA9685* driver;
         int* targetAngle;
         int* speed;
         bool* turning;
         bool* running;
     };
-    extern ThreadParameters thread_parameters;
-    int targetAngle;
-    int speed;
-    int channel;
-    PCA9685 * driver;
+    extern ThreadParameters thread_parameters; // init constructor
+    int targetAngle; // init constructor
+    int speed; // init constructor
+    int channel; // init constructor
+    PCA9685 * driver; // init constructor
     void* thread_method(void* args);
-    int max_angle;
-    int min_angle;
-    int max_length_mod;
-    int min_length_mod;
+    int max_angle; // init in constructor parameters
+    int min_angle; // init in constructor parameters
+    int max_length_mod; // init constructor
+    int min_length_mod; // init constructor
     const int MAX_LENGTH = 412;
     const int MIN_LENGTH = 204;
     const int PERIOD = 19920;
     const int FREQ = 45;
     const int FULL_RESOLUTION = 4096;
-    const int WIDTH_RESOLUTION = MAX_LENGTH - MIN_LENGTH;
-    pthread_t thread;
-    bool running;
-    bool turning;
-    int servo_angle;
-    int servo_speed;
+    const int WIDTH_RESOLUTION = 208;
+    pthread_t thread; // init & start constructor
+    bool running; // init constructor
+    bool turning; // init constructor
+    int servo_angle; // init constructor
+    int servo_speed; // init constructor
 
 };
 #endif
