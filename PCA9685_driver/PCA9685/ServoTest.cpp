fbd408d90f050be2c572b37256693c3bf23395a4
Sietsewer <sietsewer@gmail.com>
2014-06-03 01:50:52 +0200

    drivers update

---
+++ PCA9685_driver/PCA9685/ServoTest.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstddef>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "PCA9685.h"
+#include "Servo.h"
+
+using namespace std;
+
+int main() {
+    const uint8_t ADDRESS = 0x40;
+    const uint8_t BUS_NUM = 1;
+    PCA9685 driver = PCA9685();
+    driver.init(BUS_NUM, ADDRESS);
+    driver.setPWMFreq(45);
+
+    Servo* servos[3];
+    servos[0] = new Servo(0, &driver);
+    servos[1] = new Servo(3, &driver);
+    servos[2] = new Servo(4, &driver);
+
+    bool b = true;
+
+    servos[0]->setAngle(0, 1000);
+    servos[1]->setAngle(0, 1000);
+    servos[2]->setAngle(0, 1000);
+
+    int s;
+    int i = 0;
+    int t = 0;
+    int val = 0;
+
+    while (b) {
+        cout << "Command (1 = setangle  2 = walk): " << endl;
+        cin >> s;
+        switch (s) {
+            case 1:
+                cout << "Index: ";
+                cin >> i;
+                cout << " Angle: ";
+                cin >> val;
+                cout << " Time: ";
+                cin >> t;
+                servos[i]->setAngle(val, t);
+                cout << endl << "Set " << i << " to " << val << " in " << t << "." << endl;
+                break;
+            case 2:
+                cout << "Not dun yet." << endl;
+                break;
+            case 3:
+                b = false;
+                cout << "Stopping" << endl;
+                break;
+            default:
+                cout << "Invalid." << endl;
+                break;
+        }
+
+    }
+
+    for (int i = 0; i < 3; i++) {
+        delete servos[i];
+    }
+
+    return 0;
+}
---
+++ PCA9685_driver/PCA9685/build.sh
@@ -0,0 +1,2 @@
+g++ ServoTest.cpp PCA9685.cpp Servo.cpp -lpthread -o servotest 
+   #-std=c++0x
